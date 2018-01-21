be52f660b426721a9ee40407a2645723f0848288
Tyler Sanford <tylersanford0311@gmail.com>
2018-01-21 09:33:14 -0600

    Working fix for dotenv Heroku/Local environments!

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,15 @@
-require('dotenv').config();
 import axios from 'axios';
 
 export const GET_RECIPES = 'GET_RECIPES';
 
-if (process.env.NODE_ENV === 'production') {
-  const endpoint = '/api/recipes';
-} else {
-  const endpoint = 'http://localhost:4000/api/recipes';
-}
+console.log(process.env.NODE_ENV);
+const endpoint = '/api/recipes';
+
+// if (process.env.NODE_ENV === 'production') {
+//   const endpoint = '/api/recipes';
+// } else {
+// const endpoint = 'http://localhost:4000/api/recipes';
+// }
 
 export const getRecipes = () => {
   const recipesRequest = axios.get(endpoint);
