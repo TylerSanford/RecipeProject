095198407d20eef0f4d1a3fe4e29173a03e6a6ce
Tyler Sanford <tylersanford0311@gmail.com>
2018-01-24 21:44:17 -0600

    reactstrap testing 2

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './recipe.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import {
   Card,
+  Button,
   CardImg,
   CardBody,
   CardTitle,
@@ -15,32 +16,44 @@ import {
   Collapse
 } from 'reactstrap';
 
-const Recipe = props => {
-  return (
-    <Card
-      className="border border-dark"
-      style={{ width: '400px', margin: '10px' }}
-    >
-      <CardHeader>{props.name}</CardHeader>
-      <CardImg
-        top
-        width="100%"
-        src="https://thumb1.shutterstock.com/display_pic_with_logo/1535435/446808100/stock-photo-assorted-chinese-food-set-chinese-noodles-fried-rice-dumplings-peking-duck-dim-sum-spring-446808100.jpg"
-        className="rounded-bottom rounded-0 rounded-bottom"
-      />
-      <CardBody>
-        <Collapse isOpen="false">
-          <b>Ingredients</b>
-          <CardText>{props.ingredients}</CardText>
-          <b>Instructions</b>
-          <CardText>{props.instructions}</CardText>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+class Recipe extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      toggled: false
+    };
+  }
+
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  render() {
+    return (
+      <Card
+        onClick={this.toggle}
+        className="border border-dark"
+        style={{ width: '400px', margin: '10px' }}
+      >
+        <CardHeader>{this.props.name}</CardHeader>
+        <CardImg
+          top
+          width="100%"
+          src="https://thumb1.shutterstock.com/display_pic_with_logo/1535435/446808100/stock-photo-assorted-chinese-food-set-chinese-noodles-fried-rice-dumplings-peking-duck-dim-sum-spring-446808100.jpg"
+          className="rounded-top-0 rounded-bottom"
+        />
+        <Collapse isOpen={this.state.collapse}>
+          <CardBody>
+            <b>Ingredients</b>
+            <CardText>{this.props.ingredients}</CardText>
+            <b>Instructions</b>
+            <CardText>{this.props.instructions}</CardText>
+          </CardBody>
         </Collapse>
-      </CardBody>
-    </Card>
-  );
-};
+      </Card>
+    );
+  }
+}
 
 export default Recipe;
