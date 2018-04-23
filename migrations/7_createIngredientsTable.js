4c39f290cf6729c1e9293c87beeea6b14ec2a054
Tyler Sanford <tylersanford0311@gmail.com>
2018-04-23 12:17:23 -0500

    Remove abbreviation from IngredientsTable, rename field('name') to 'measurement'

--- migrations/7_createIngredientsTable.js
+++ migrations/7_createIngredientsTable.js
@@ -1,8 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('ingredients', function(tbl) {
     tbl.increments('id').primary(); // primary key
-		tbl.string('name').notNullable();
-		tbl.string('abbreviation').notNullable();
+		tbl.string('measurement').notNullable();
   });
 };
 
