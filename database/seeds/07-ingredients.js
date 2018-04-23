80deef9bd9797f385b9fd537d3981bd7ac8878f9
Tyler Sanford <tylersanford0311@gmail.com>
2018-04-23 12:28:35 -0500

    Measurements Migration - Rename field('name') to 'measurement'

--- database/seeds/07-ingredients.js
+++ database/seeds/07-ingredients.js
@@ -10,40 +10,40 @@ exports.seed = function(knex, Promise) {
           // 1
           name: 'All-Purpose Flour'
         },
-        {
+        { // 2
           name: 'Baking Soda'
         },
-        {
+        { // 3
           name: 'Salt'
         },
-        {
+        { // 4
           name: 'Butter'
         },
-        {
+        { // 5
           name: 'Brown Sugar'
         },
-        {
+        { // 6
           name: 'Granulated Sugar'
         },
-        {
+        { // 7
           name: 'Vanilla Extract'
         },
-        {
+        { // 8
           name: 'Large Eggs'
         },
-        {
+        { // 9
           name: 'Nestle Toll House Semi-Sweet Chocolate Chips'
         },
-        {
+        { // 10
           name: 'Chopped Nuts'
         },
-        {
+        { // 11
           name: 'Bacon'
         },
-        {
+        { // 12
           name: "Jalapeno's"
         },
-        {
+        { // 13
           name: 'Cream Cheese'
         }
       ]);
--- migrations/6_createMeasurementsTable.js
+++ migrations/6_createMeasurementsTable.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('measurements', function(tbl) {
     tbl.increments('id').primary(); // primary key
-    tbl.string('name').notNullable();
+    tbl.string('measurement').notNullable();
     tbl.string('abbreviation').notNullable();
   });
 };
